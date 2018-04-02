659b87a0de89ecdd93fc521557cfb5a6a6fbbff5
patty8909 <patty90_30@hotmail.com>
2018-04-02 11:01:09 -0500

    Updates

--- src/register/ShowResults.js
+++
@@ -1,8 +0,0 @@
-const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
-
-export default (async function showResults(values) {
-  await sleep(500); // simulate server latency
-  console.log(`You submitted:
-  ${JSON.stringify(values, null, 2)}`);
-  localStorage.setItem ('firstPerson', JSON.stringify(values, null, 2))
-});
