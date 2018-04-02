11de1934123e595fd8c38e59caef2662cf00bedb
patty8909 <patty90_30@hotmail.com>
2018-04-02 07:24:39 -0500

    añade react-router-dom

--- src/Zone/PrivateZone.js
+++ src/Zone/PrivateZone.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Link, Switch, Route, NavLink, withRouter } from 'react-router-dom';
+import ChangeBox from './changeBox'
 
 const PrivateZone = props =>  {
   const { handleSubmit } = props;
   const local = JSON.parse(localStorage.getItem("firstPerson"))
   return (
     // debería iterarse con map para que con cada click acceda a la ruta onClick={() => navigateTo(id)}>
-  
+    <Router>
   <div className="col-md-3 md-mb-2 justify-content-center align-items-center">
     <p><strong>Cuenta Personal</strong></p>
     <p>{`DNI ${local.documentNumber}`}</p>
     <ul className="list-group sidebar-nav mb-4" id="sidebar-nav">
       <li className="d-flex align-items-center">
-        <a href="#/"><i className="fa fa-play"></i> Nueva Operación</a>
+        <a href="#"><Link to='/changebox'><i className="fa fa-play"></i> Nueva Operación</Link></a>
       </li>
       <li className="list-group-item active">
         <a href="#/"><i className="fa fa-bar-chart-o"></i> Principal</a>
@@ -33,7 +35,12 @@ const PrivateZone = props =>  {
         <a href="#/"><i className="fa fa-credit-card"></i> Cuentas Bancarias de Rextie</a>
       </li>
     </ul>
+
+    <Switch>
+            <Route path='/changebox' component={ChangeBox} /> 
+          </Switch>
   </div>
+  </Router>
   );
 }
 
