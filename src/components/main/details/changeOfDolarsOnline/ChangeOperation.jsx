16db495f944a8ca9ab74099efc4add67d60d81a0
patty8909 <patty90_30@hotmail.com>
2018-04-03 18:29:04 -0500

    añade funcionalidad input

--- src/components/main/details/changeOfDolarsOnline/ChangeOperation.jsx
+++ src/components/main/details/changeOfDolarsOnline/ChangeOperation.jsx
@@ -1,66 +1,86 @@
 import React from 'react';
+import { Component } from 'react';
 
-const ChangeOperation = () => (
-  <div>
-    <div className="Container p-3 col-12 col-lg-9">
-  <div className="row justify-content-md-center">
-    <div className="card border-info mb-3" style={{'maxWidth': '18rem'}}>
-      <div className="card-body text-info">
-        <h3>Compra: 3.2000
-          <span></span>
-          &nbsp;-&nbsp;Venta: 3.2480<span></span>
-        </h3>
+class ChangeOperation extends Component {
+  constructor() {
+    super();
+    this.state = {
+      soles:''
+    }
+  }
+  render() {
+    return(
+      <div>
+      <div className="Container p-3 col-12 col-lg-9">
+    <div className="row justify-content-md-center">
+      <div className="card border-info mb-3" style={{'maxWidth': '18rem'}}>
+        <div className="card-body text-info">
+          <h3>Compra: 3.2000
+            <span></span>
+            &nbsp;-&nbsp;Venta: 3.2480<span></span>
+          </h3>
+        </div>
+      </div>
+      </div>
+      </div>
+  
+      <div className="container">
+    <div className="row justify-content-md-center">
+      <div className="col col-md-2 change">
+        Tengo
+        <input type="number" value={this.state.soles} onChange={this.update.bind(this)}/>
+      </div>
+      <div className="col col-md-2 change">
+        USD $
       </div>
     </div>
-    </div>
-    </div>
-
-    <div className="container">
-  <div className="row justify-content-md-center">
-    <div className="col col-md-2 change">
-      Tengo
-    </div>
-    <div className="col col-md-2 change">
-      USD $
+    <div className="row justify-content-md-center">
+      <div className="col col-md-2 change">
+        Quiero
+        <p>{Math.round(this.state.soles / 3.2480) }</p>
+      </div>
+      <div className="col col-md-2 change">
+        PEN S/.
+      </div>
+     
     </div>
   </div>
-  <div className="row justify-content-md-center">
-    <div className="col col-md-2 change">
-      Quiero
-    </div>
-    <div className="col col-md-2 change">
-      PEN S/.
-    </div>
-   
-  </div>
-</div>
-    
-    <div className="container">
-      <div className="col-sm-6">
-        <form className="dropdown-menu p-4">
-          <div className="form-group">
-            <label htmlFor="exampleDropdownFormEmail2">Email address</label>
-            <input type="email" className="form-control" id="exampleDropdownFormEmail2" placeholder="email@example.com"/>
-          </div>
-          <div className="form-group">
-            <label htmlFor="exampleDropdownFormPassword2">Password</label>
-            <input type="password" className="form-control" id="exampleDropdownFormPassword2" placeholder="Password"/>
-          </div>
-          <div className="form-check">
-            <input type="checkbox" className="form-check-input" id="dropdownCheck2"/>
-            <label className="form-check-label" htmlFor="dropdownCheck2">
-              Remember me
-            </label>
-          </div>
-          <button type="submit" className="btn btn-primary">
-            <div id="currencyimage" alt="">
-              <img src="../../assets/currency-rates.svg" alt="currency"/>
+      
+      <div className="container">
+        <div className="col-sm-6">
+          <form className="dropdown-menu p-4">
+            <div className="form-group">
+              <label htmlFor="exampleDropdownFormEmail2">Email address</label>
+              <input type="email" className="form-control" id="exampleDropdownFormEmail2" placeholder="email@example.com"/>
             </div>
-          </button>
-        </form>
+            <div className="form-group">
+              <label htmlFor="exampleDropdownFormPassword2">Password</label>
+              <input type="password" className="form-control" id="exampleDropdownFormPassword2" placeholder="Password"/>
+            </div>
+            <div className="form-check">
+              <input type="checkbox" className="form-check-input" id="dropdownCheck2"/>
+              <label className="form-check-label" htmlFor="dropdownCheck2">
+                Remember me
+              </label>
+            </div>
+            <button type="submit" className="btn btn-primary">
+              <div id="currencyimage" alt="">
+                <img src="../../assets/currency-rates.svg" alt="currency"/>
+              </div>
+            </button>
+          </form>
+        </div>
       </div>
     </div>
-  </div>
-);
+    )}
+
+    update(event) {
+      this.setState({
+        soles: event.target.value
+      })
+    }
+}
+
+
 
 export default ChangeOperation;
