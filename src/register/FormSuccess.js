af6bb56e13c867ff2a5bbe29e50cde9310f26103
LeslyN <lnombertoc@gmail.com>
2018-04-04 17:39:10 -0500

    cambios listos

--- src/register/FormSuccess.js
+++ src/register/FormSuccess.js
@@ -6,18 +6,14 @@ const FormSuccess = props => {
   const { handleSubmit } = props;
   return ( 
     <form onSubmit={handleSubmit}>
-    <div class="alert alert-success" role="alert">
-      <h4 class="alert-heading">Felicidades ! Estás registrado en Rextie.</h4>
-      <p>Nuestro horario de atención es de Lunes a Sábado de 9 AM a 6 PM.</p>
-      <p class="mb-0">En caso de duda o urgencia, contacta por chat o email info@rextie.com</p>
-      <hr/>
-      <div>
-<<<<<<< HEAD
-      <button type="submit" className="next" >Ir a página principal</button>
-=======
-        <button type="submit" className="next btn btn-outline-info" >Ir a página principal</button>
-      </div>
->>>>>>> 8b56b7fc6e568e7ae5fc57a3099a55a83d9730f0
+      <div class="alert alert-success" role="alert">
+        <h4 class="alert-heading">Felicidades ! Estás registrado en Rextie.</h4>
+        <p>Nuestro horario de atención es de Lunes a Sábado de 9 AM a 6 PM.</p>
+        <p class="mb-0">En caso de duda o urgencia, contacta por chat o email info@rextie.com</p>
+        <hr/>
+        <div>
+        <button type="submit" className="next" >Ir a página principal</button>
+        </div>
       </div>
     </form>
 
